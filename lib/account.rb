eb1e49a7634c1a9efe528bd68740df7b49fc2d4b
Samir Gossain <samir.gossain@gmail.com>
2017-03-06 16:33:38 +0000

    Refactored AccStatement Method, yet to add Test

--- lib/account.rb
+++ lib/account.rb
@@ -18,16 +18,8 @@ class Account
     @summary.push transaction
   end
 
-  # def show_statement
-  #   puts 'Date || Transaction || Amount || Balance'
-  #   print @summary[0][0].to_s + '||' + @summary[0][1].to_s + '||' + @summary[0][2].to_s + '||' + @summary[0][3].to_s + '||' + "\n"
-  #   print @summary[1][0].to_s + '||' + @summary[1][1].to_s + '||' + @summary[1][2].to_s + '||' + @summary[1][3].to_s + '||' + "\n"
-  #   print @summary[2][0].to_s + '||' + @summary[2][1].to_s + '||' + @summary[2][2].to_s + '||' + @summary[2][3].to_s + '||' + "\n"
-  #   print @summary[3][0].to_s + '||' + @summary[3][1].to_s + '||' + @summary[3][2].to_s + '||' + @summary[3][3].to_s + '||' + "\n"
-  # end
-
   def show_statement
-    puts 'Date || Transaction || Amount || Balance ||'
+    puts 'Date      ||Deb/Cr||Amt||Bal||'
     @summary.each do |line|
       line.each do |x|
         print x.to_s + "||"
